1fd130ee2024ec2a3239c205987ebe8d1f39f39c
vdantr <veselin.trifonov.tt@gmail.com>
2023-02-14 18:22:43 +0100

    navbar ready

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+    // <h1>App</h1>
+  )
+}
