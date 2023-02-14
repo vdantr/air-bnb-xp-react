70017c762637d08e7ecd0104e3f8b94f80510d4e
vdantr <veselin.trifonov.tt@gmail.com>
2023-02-14 18:44:38 +0100

    add hero component

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
     // <h1>App</h1>
   )
